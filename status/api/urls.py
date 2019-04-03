2369cd760af7722a02dc2ef1e39f49732564d9d6
Vojta Janousek <janousek315@gmail.com>
2019-04-03 20:48:36 +0200

    added method testing script

--- status/api/urls.py
+++ status/api/urls.py
@@ -3,7 +3,7 @@ from status.api import views
 
 urlpatterns = [
     # re_path(r'^create/$', views.StatusCreateAPIView.as_view()),
-    re_path(r'^(?P<pk>\d+)/$', views.StatusDetailAPIView.as_view()),
+    # re_path(r'^(?P<pk>\d+)/$', views.StatusDetailAPIView.as_view()),
     # re_path(r'^(?P<pk>\d+)/update/$', views.StatusUpdateAPIView.as_view()),
     # re_path(r'^(?P<pk>\d+)/delete/$', views.StatusDeleteAPIView.as_view()),
     re_path(r'^$', views.StatusAPIView.as_view()),
--- status/api/views.py
+++ status/api/views.py
@@ -1,29 +1,47 @@
+import json
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics, mixins
+from django.shortcuts import get_object_or_404
 
 from .serializers import StatusSerializer
 from status.models import Status
 
-class StatusListSearchAPIView(APIView):
-    permission_classes = []
-    authentication_classes = []
+# class StatusListSearchAPIView(APIView):
+#     permission_classes = []
+#     authentication_classes = []
+#
+#     def get(self, request, format=None):
+#         qs = Status.objects.all()
+#         serializer = StatusSerializer(qs, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, format=None):
+#         qs = Status.objects.all()
+#         serializer = StatusSerializer(qs, many=True)
+#         return Response(serializer.data)
 
-    def get(self, request, format=None):
-        qs = Status.objects.all()
-        serializer = StatusSerializer(qs, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        qs = Status.objects.all()
-        serializer = StatusSerializer(qs, many=True)
-        return Response(serializer.data)
+def is_json(json_data):
+    try:
+        real_json = json.loads(json_data)
+        is_valid = True
+    except ValueError:
+        is_valid = False
+    return is_valid
 
 
-class StatusAPIView(mixins.CreateModelMixin, generics.ListAPIView):
+class StatusAPIView(
+        mixins.CreateModelMixin,
+        mixins.RetrieveModelMixin,
+        mixins.UpdateModelMixin,
+        mixins.DestroyModelMixin,
+        generics.ListAPIView):
+
     permission_classes = []
     authentication_classes = []
     serializer_class = StatusSerializer
+    passed_id = None
 
     def get_queryset(self):
         qs = Status.objects.all()
@@ -32,8 +50,76 @@ class StatusAPIView(mixins.CreateModelMixin, generics.ListAPIView):
             qs = qs.filter(summary__icontains=query)
         return qs
 
+    def get_object(self):
+        request = self.request
+        passed_id = request.GET.get('id', None) or self.passed_id
+        queryset = self.get_queryset()
+        obj = None
+        if passed_id is not None:
+            obj = get_object_or_404(queryset, id=passed_id)
+            self.check_object_permissions(request, obj)
+        return obj
+
+    def get(self, request, *args, **kwargs):
+        url_passed_id = request.GET.get('id', None)
+
+        json_data = {}
+        body_ = request.body
+        if is_json(body_):
+            json_data = json.loads(request.body)
+        new_passed_id = json_data.get('id', None)
+
+        passed_id = url_passed_id or new_passed_id or None
+        self.passed_id = passed_id
+
+        if passed_id is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return super().get(request, *args, **kwargs)
+
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        url_passed_id = request.GET.get('id', None)
+
+        json_data = {}
+        body_ = request.body
+        if is_json(body_):
+            json_data = json.loads(request.body)
+        new_passed_id = json_data.get('id', None)
+
+        passed_id = url_passed_id or new_passed_id or None
+        self.passed_id = passed_id
+
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        url_passed_id = request.GET.get('id', None)
+
+        json_data = {}
+        body_ = request.body
+        if is_json(body_):
+            json_data = json.loads(request.body)
+        new_passed_id = json_data.get('id', None)
+
+        passed_id = url_passed_id or new_passed_id or None
+        self.passed_id = passed_id
+
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        url_passed_id = request.GET.get('id', None)
+
+        json_data = {}
+        body_ = request.body
+        if is_json(body_):
+            json_data = json.loads(request.body)
+        new_passed_id = json_data.get('id', None)
+
+        passed_id = url_passed_id or new_passed_id or None
+        self.passed_id = passed_id
+
+        return self.destroy(request, *args, **kwargs)
 
 
 
@@ -43,17 +129,17 @@ class StatusAPIView(mixins.CreateModelMixin, generics.ListAPIView):
 
 
 
-class StatusDetailAPIView(mixins.DestroyModelMixin, mixins.UpdateModelMixin, generics.RetrieveAPIView):
-    permission_classes = []
-    authentication_classes = []
-    queryset = Status.objects.all()
-    serializer_class = StatusSerializer
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+# class StatusDetailAPIView(mixins.DestroyModelMixin, mixins.UpdateModelMixin, generics.RetrieveAPIView):
+#     permission_classes = []
+#     authentication_classes = []
+#     queryset = Status.objects.all()
+#     serializer_class = StatusSerializer
+#
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+#
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
 
 
 # class StatusUpdateAPIView(generics.UpdateAPIView):
