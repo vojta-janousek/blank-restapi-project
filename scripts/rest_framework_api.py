c3d17ebe96c602022e396649d4d4181592d221d5
Vojta Janousek <janousek315@gmail.com>
2019-04-15 23:22:20 +0200

    nested serializers

--- scripts/rest_framework_api.py
+++ scripts/rest_framework_api.py
@@ -36,7 +36,7 @@ with open(image_path, 'rb') as image:
     file_data = {
         'image': image,
     }
-    r = requests.put(ENDPOINT, data=data2, headers=headers2, files=file_data)
+    r = requests.get(ENDPOINT, headers=headers2)
     print(r.text)
     # r = requests.post(BASE_ENDPOINT, data=data2, headers=headers2, files=file_data)
     # print(r.text)
--- status/api/serializers.py
+++ status/api/serializers.py
@@ -1,18 +1,23 @@
 from rest_framework import serializers
 
 from status.models import Status
+from accounts.api.serializers import UserPublicSerializer
 
 
 class StatusSerializer(serializers.ModelSerializer):
-
+    uri = serializers.SerializerMethodField(read_only=True)
+    user = UserPublicSerializer(read_only=True)
     class Meta:
         model = Status
-        fields = ('id', 'user', 'summary', 'image')
+        fields = ('uri', 'id', 'user', 'summary', 'image')
         read_only_fields = ['user']
 
-    def validate_content(self, value):
-        if len(value) > 1000:
-            raise serializers.ValidationError("This is way too long.")
+    def get_uri(self, obj):
+        return "/api/status/{id}/".format(id=obj.id)
+
+    # def validate_content(self, value):
+    #     if len(value) > 1000:
+    #         raise serializers.ValidationError("This is way too long.")
 
     def validate(self, data):
         summary = data.get('summary', None)
